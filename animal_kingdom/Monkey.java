e0e9d9dd4bd952309340b3b3cb3db7e53726d3b8
Travis Grimmond <travisslack1994@outlook.com>
2017-08-11 17:43:02 +0100

    edited monkey to have age

--- animal_kingdom/Monkey.java
+++ animal_kingdom/Monkey.java
@@ -2,8 +2,8 @@ package animal_kingdom;
 
 public class Monkey extends Animal {
 
-  public class Monkey(String name, String colour) {
-    super(name, colour);
+  public class Monkey(String name, String colour, int age) {
+    super(name, colour, age);
   }
 
 }
