95a0a9b541f7628caeeee6a9db6d465a44d74d55
Travis Grimmond <travisslack1994@outlook.com>
2017-08-11 17:42:20 +0100

    New monkey details added into Test

--- animal_kingdom/Animal.java
+++ animal_kingdom/Animal.java
@@ -3,11 +3,15 @@ package animal_kingdom;
 public abstract class Animal {
   protected String name;
   protected String colour;
+  protected int age;
 
-  public Animal(String name, String colour) {
+  public Animal(String name, String colour, int age) {
     this.name = name;
     this.colour = colour;
+    this.age = age;
   }
+
+  
 
   
 }
---
+++ animal_kingdom_specs/MonkeyTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import animal_kingdom.*;
+
+public class MonkeyTest {
+  Monkey monkey
+
+  @Before
+  public void before() {
+    monkey = new Monkey("Julius", "brown", 4)
+  }
+}
