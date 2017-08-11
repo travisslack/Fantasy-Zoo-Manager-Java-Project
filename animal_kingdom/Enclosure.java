4eebcd5aee5be9e8309e63eccceaf325d26b47bb
Travis Grimmond <travisslack1994@outlook.com>
2017-08-11 15:32:06 +0100

    created an enclosure file filled with name and capacity

--- animal_kingdom/Enclosure.java
+++ animal_kingdom/Enclosure.java
@@ -1,3 +1,14 @@
 package animal_kingdom;
 import java.util.ArrayList;
 
+public abstract class Enclosure {
+  protected String name;
+  protected int capacity;
+
+  public Enclosure(String name, int capacity) {
+    this.name = name;
+    this.capacity = capacity;
+  }
+
+  
+}
