8e45b74f027fad77121cdaa5d24d7033b5daf3d0
Travis Grimmond <travisslack1994@outlook.com>
2017-08-14 12:20:45 +0100

    ZooKeeper can now add animal to the specific Enclosure

--- animal_kingdom/ZooKeeper.java
+++ animal_kingdom/ZooKeeper.java
@@ -4,12 +4,17 @@ import java.util.*;
 public class ZooKeeper {
   private String name;
 
+
   public ZooKeeper(String name) {
     this.name = name;
   }
 
   public String getName() {
     return this.name;
+  }
+
+  public void addToEnclosure(Animal animal, Enclosure enclosure) {
+     enclosure.addToEnclosure(animal);
   }
 }
 
--- animal_kingdom_specs/ZooKeeperTest.java
+++ animal_kingdom_specs/ZooKeeperTest.java
@@ -4,15 +4,26 @@ import animal_kingdom.*;
 
 public class ZooKeeperTest {
   ZooKeeper zooKeeper;
-
+  Monkey monkey;
+  MonkeyEnclosure monkeyEnclosure;
+  
   @Before
   public void before() {
     zooKeeper = new ZooKeeper("David");
-
+    monkeyEnclosure = new MonkeyEnclosure("Monkey Enclosure", 15);
+    monkey = new Monkey("Julius", "Brown", 4);
   }
 
   @Test
   public void hasName() {
     assertEquals("David", zooKeeper.getName());
   }
+
+  @Test
+  public void addAnimalToEnclosure(){
+    zooKeeper.addToEnclosure(monkey, monkeyEnclosure);
+    assertEquals(1, monkeyEnclosure.animalCount());
+  }
+
+
 }
