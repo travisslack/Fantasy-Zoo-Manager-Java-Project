658c951bbf10527d75044e4eb45cfee7a30d82bb
Travis Grimmond <travisslack1994@outlook.com>
2017-08-14 13:59:26 +0100

    Added a Plants eating class so can now add food, feed the animals

--- animal_kingdom/Monkey.java
+++ animal_kingdom/Monkey.java
@@ -1,9 +1,11 @@
 package animal_kingdom;
 
 public class Monkey extends Animal {
+  private ArrayList<Plants> stomach;
 
   public Monkey(String name, String colour, int age) {
     super(name, colour, age);
+    this.stomach = new ArrayList<Plants>();
   }
 
   public String getName() {
--- animal_kingdom_specs/MonkeyTest.java
+++ animal_kingdom_specs/MonkeyTest.java
@@ -4,10 +4,14 @@ import animal_kingdom.*;
 
 public class MonkeyTest {
   Monkey monkey;
+  Plant plant;
+  Banana banana;
 
   @Before
   public void before() {
     monkey = new Monkey("Julius", "Brown", 4);
+    plant = new Plant();
+    banana = new Banana();
   }
 
   @Test
