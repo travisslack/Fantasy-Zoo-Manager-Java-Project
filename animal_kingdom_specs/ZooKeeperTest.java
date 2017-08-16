e0d25e313195c978b985d4ab34de6672c1871f68
Travis Grimmond <travisslack1994@outlook.com>
2017-08-16 17:32:31 +0100

    All tests working and not commented out

--- animal_kingdom_specs/ZooKeeperTest.java
+++ animal_kingdom_specs/ZooKeeperTest.java
@@ -24,61 +24,61 @@ public class ZooKeeperTest {
     nut = new Nut();
   }
 
-  // @Test
-  // public void hasName() {
-  //   assertEquals("David", edinburghZooKeeper.getName());
-  // }
+  @Test
+  public void hasName() {
+    assertEquals("David", edinburghZooKeeper.getName());
+  }
 
-  // @Test
-  // public void canAddMonkeyToMonkeyEnclosure(){
-  //   edinburghZooKeeper.addToEnclosure(monkey, edinburghMonkeyEnclosure);
-  //   assertEquals(1, edinburghMonkeyEnclosure.animalCount());
-  // }
+  @Test
+  public void canAddMonkeyToMonkeyEnclosure(){
+    edinburghZooKeeper.addToEnclosure(monkey, edinburghMonkeyEnclosure);
+    assertEquals(1, edinburghMonkeyEnclosure.animalCount());
+  }
 
-  // @Test
-  // public void canAddSnakeToSnakeEnclosure(){
-  //   edinburghZooKeeper.addToEnclosure(snake, edinburghSnakeEnclosure);
-  //   assertEquals(1, edinburghSnakeEnclosure.animalCount());
-  // }
+  @Test
+  public void canAddSnakeToSnakeEnclosure(){
+    edinburghZooKeeper.addToEnclosure(snake, edinburghSnakeEnclosure);
+    assertEquals(1, edinburghSnakeEnclosure.animalCount());
+  }
 
-  // @Test
-  // public void canAddMonkeyToSnakeEnclosure() {
-  //   edinburghSnakeEnclosure.addToEnclosure(monkey);
-  //   assertEquals(1, edinburghSnakeEnclosure.animalCount());
-  // }
+  @Test
+  public void canAddMonkeyToSnakeEnclosure() {
+    edinburghSnakeEnclosure.addToEnclosure(monkey);
+    assertEquals(1, edinburghSnakeEnclosure.animalCount());
+  }
 
-  // @Test
-  // public void canRemoveMonkeyFromMonkeyEnclosure() {
-  //   edinburghZooKeeper.addToEnclosure(monkey, edinburghMonkeyEnclosure);
-  //   edinburghZooKeeper.removeFromEnclosure(monkey, edinburghMonkeyEnclosure);
-  //   assertEquals(0, edinburghMonkeyEnclosure.animalCount());
-  // }
+  @Test
+  public void canRemoveMonkeyFromMonkeyEnclosure() {
+    edinburghZooKeeper.addToEnclosure(monkey, edinburghMonkeyEnclosure);
+    edinburghZooKeeper.removeFromEnclosure(monkey, edinburghMonkeyEnclosure);
+    assertEquals(0, edinburghMonkeyEnclosure.animalCount());
+  }
 
-  // @Test
-  // public void canRemoveSnakeFromSnakeEnclosure() {
-  //   edinburghZooKeeper.addToEnclosure(snake, edinburghSnakeEnclosure);
-  //   edinburghZooKeeper.removeFromEnclosure(snake, edinburghSnakeEnclosure);
-  //   assertEquals(0, edinburghSnakeEnclosure.animalCount());
-  // }
+  @Test
+  public void canRemoveSnakeFromSnakeEnclosure() {
+    edinburghZooKeeper.addToEnclosure(snake, edinburghSnakeEnclosure);
+    edinburghZooKeeper.removeFromEnclosure(snake, edinburghSnakeEnclosure);
+    assertEquals(0, edinburghSnakeEnclosure.animalCount());
+  }
 
-  // @Test
-  // public void canFeedBananaToMonkey() {
-  //   edinburghZooKeeper.feedAnimal(banana, monkey);
-  //   assertEquals(1, monkey.foodCount());
-  // }
+  @Test
+  public void canFeedBananaToMonkey() {
+    edinburghZooKeeper.feedAnimal(banana, monkey);
+    assertEquals(1, monkey.foodCount());
+  }
 
-  // @Test
-  // public void canFeedNutToSnake() {
-  //   edinburghZooKeeper.feedAnimal(nut, snake);
-  //   assertEquals(1, snake.foodCount());
-  // }
+  @Test
+  public void canFeedNutToSnake() {
+    edinburghZooKeeper.feedAnimal(nut, snake);
+    assertEquals(1, snake.foodCount());
+  }
 
-  // @Test
-  // public void canFeedMonkeyToSnake() {
-  //   edinburghZooKeeper.addToEnclosure(monkey, edinburghSnakeEnclosure);
-  //   edinburghZooKeeper.feedAnimal(monkey, snake);
-  //   assertEquals(1, snake.foodCount());
-  // }
+  @Test
+  public void canFeedMonkeyToSnake() {
+    edinburghZooKeeper.addToEnclosure(monkey, edinburghSnakeEnclosure);
+    edinburghZooKeeper.feedAnimal(monkey, snake);
+    assertEquals(1, snake.foodCount());
+  }
 
   @Test
   public void canHouseAnimal(){
